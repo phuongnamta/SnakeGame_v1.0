893ab7480f4e481436eee2372a3a367d2ee92efa
Phuong Nam Ta <phuongnamta2002@gmail.com>
2024-07-27 14:55:40 +0700

    up load code

---
+++ main.cpp
@@ -0,0 +1,182 @@
+#include <iostream>
+#include <conio.h>  // For Windows, use <ncurses.h> for Linux
+#include <windows.h>
+
+using namespace std;
+
+bool gameOver;
+const int width = 20;
+const int height = 20;
+int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int nTail;
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
+eDirection dir;
+
+void Setup() {
+    gameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
+    nTail = 0;
+}
+
+void Draw() {
+    system("cls");  // Use "clear" for Linux
+    for (int i = 0; i < width + 2; i++)
+        cout << "#";
+    cout << endl;
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (j == 0)
+                cout << "#";
+            if (i == y && j == x)
+                cout << "O";
+            else if (i == fruitY && j == fruitX)
+                cout << "F";
+            else {
+                bool print = false;
+                for (int k = 0; k < nTail; k++) {
+                    if (tailX[k] == j && tailY[k] == i) {
+                        cout << "o";
+                        print = true;
+                    }
+                }
+                if (!print)
+                    cout << " ";
+            }
+
+            if (j == width - 1)
+                cout << "#";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < width + 2; i++)
+        cout << "#";
+    cout << endl;
+    cout << "Score: " << score << endl;
+}
+
+void Input() {
+    if (_kbhit()) {
+        switch (_getch()) {
+        case 'a':
+            dir = LEFT;
+            break;
+        case 'd':
+            dir = RIGHT;
+            break;
+        case 'w':
+            dir = UP;
+            break;
+        case 's':
+            dir = DOWN;
+            break;
+        case 'x':
+            gameOver = true;
+            break;
+        }
+    }
+}
+
+void Logic() {
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
+    for (int i = 1; i < nTail; i++) {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+    switch (dir) {
+    case LEFT:
+        x--;
+        break;
+    case RIGHT:
+        x++;
+        break;
+    case UP:
+        y--;
+        break;
+    case DOWN:
+        y++;
+        break;
+    default:
+        break;
+    }
+
+    if (x >= width) x = 0; else if (x < 0) x = width - 1;
+    if (y >= height) y = 0; else if (y < 0) y = height - 1;
+
+    for (int i = 0; i < nTail; i++)
+        if (tailX[i] == x && tailY[i] == y)
+            gameOver = true;
+
+    if (x == fruitX && y == fruitY) {
+        score += 10;
+        fruitX = rand() % width;
+        fruitY = rand() % height;
+        nTail++;
+    }
+}
+
+void ShowMenu() {
+    cout << "====================" << endl;
+    cout << "      SNAKE GAME    " << endl;
+    cout << "====================" << endl;
+    cout << "1. Start Game" << endl;
+    cout << "2. Instructions" << endl;
+    cout << "3. Exit" << endl;
+    cout << "Enter your choice: ";
+}
+
+void Instructions() {
+    cout << "====================" << endl;
+    cout << "    INSTRUCTIONS    " << endl;
+    cout << "====================" << endl;
+    cout << "Use 'W' to move up" << endl;
+    cout << "Use 'S' to move down" << endl;
+    cout << "Use 'A' to move left" << endl;
+    cout << "Use 'D' to move right" << endl;
+    cout << "Press 'X' to exit the game" << endl;
+    cout << "Press any key to go back to menu..." << endl;
+    _getch();
+}
+
+int main() {
+    int choice;
+    while (true) {
+        ShowMenu();
+        cin >> choice;
+        switch (choice) {
+        case 1:
+            Setup();
+            while (!gameOver) {
+                Draw();
+                Input();
+                Logic();
+                Sleep(100);  // Sleep(10) for faster game, Sleep(200) for slower
+            }
+            break;
+        case 2:
+            Instructions();
+            break;
+        case 3:
+            return 0;
+        default:
+            cout << "Invalid choice! Please try again." << endl;
+            break;
+        }
+    }
+    return 0;
+}
